f8a52e2ca975809e9cf4138d5cced866535b8bc6
lishao <2388286329@qq.com>
2019-09-22 18:24:26 +0800

    update

--- app.js
+++ app.js
@@ -6,5 +6,25 @@ App({
   },
   globalData: {
    
-  }
+  },
+  // setWatcher(data, watch) { // 接收index.js传过来的data对象和watch对象
+  //   Object.keys(watch).forEach(v => { // 将watch对象内的key遍历
+  //     this.observe(data, v, watch[v]); // 监听data内的v属性，传入watch内对应函数以调用
+  //   })
+  // },
+
+  // observe(obj, key, watchFun) {
+  //   var val = obj[key]; // 给该属性设默认值
+  //   Object.defineProperty(obj, key, {
+  //     configurable: true,
+  //     enumerable: true,
+  //     set: function (value) {
+  //       val = value;
+  //       watchFun(value, val); // 赋值(set)时，调用对应函数
+  //     },
+  //     get: function () {
+  //       return val;
+  //     }
+  //   })
+  // }
 })
--- pages/dictationWord/dictationWord.js
+++ pages/dictationWord/dictationWord.js
@@ -1,16 +1,17 @@
-// pages/dictationWord/dictationWord.js
+const watch=require('../../utils/watch.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    // wordCount:5
-    maxLen:0,
+    keyboardstatus:false,
+    keyValue:'',
+    wordCount:6,
     keyboard:{
       one: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
       two: ['a', 's', 'd', 'f', 'g', 'h', 'j','k','l'],
-      three: ['z', 'x', 'c', 'v', 'b', 'n', 'm', '删除']
+      three: ['z', 'x', 'c', 'v', 'b', 'n', 'm', '删']
     }
   },
 
@@ -18,7 +19,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //getApp().setWatcher(this.data, this.watch);
+    watch.setWatcher(this); 
   },
 
   /**
@@ -44,13 +46,57 @@ Page({
   },
 
   //自定义事件
+  keyEvent(e){
+    let that=this;
+    let vals=e.currentTarget.dataset.value;
+    if (vals == '删' && that.data.keyValue.length!=0) {
+      let val = that.data.keyValue.substring(0, that.data.keyValue.length-1)
+      that.setData({
+        keyValue:val
+      })
+      return false;
+    }
+    if (that.data.keyValue.length < that.data.wordCount && vals !='删'){      
+      that.data.keyValue+=vals;
+      that.setData({
+        keyValue: that.data.keyValue
+      })
+    }
+  },
 
+  //弹起或收起键盘
+  alertKey(){
+    let that=this;
+    //console.log(1111,that.data)
+    that.setData({
+      keyboardstatus: !that.data.keyboardstatus
+    });
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
 
+  },
+
+  watch:{
+    keyValue(news){
+      let that=this;
+      let wordlist=[];
+      //console.log('look',that.data.wordCount);
+      for(let i=0;i<that.data.wordCount;i++){
+        wordlist.push('');
+      }
+      //console.log(wordlist);
+      for(let i=0;i<news.length;i++){
+        wordlist[i]=news[i]
+      }
+      
+      that.setData({
+        wordList:wordlist
+      })
+    }
   },
 
   /**
